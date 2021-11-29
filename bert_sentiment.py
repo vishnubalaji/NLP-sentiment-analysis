6a3d24c18b79c49e75a14cbcb8a25c9feca67375
Vishnu Balaji <v2b2y2k@gmail.com>
2021-11-29 08:53:22 +0530

    test prod5

--- bert_sentiment.py
+++ bert_sentiment.py
@@ -32,7 +32,7 @@ def home():
         reddit()
 
 def reddit():
-    subreddit = reddit.get_subreddit('wallstreetbets')
+    subreddit = reddit.subreddit('wallstreetbets')
     st.write('Nothing here to show. Mind your business -_-')
 
 def twitter():
